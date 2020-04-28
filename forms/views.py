009a3d638f4f6703471da83371eba1149543bcbb
kollyuchka <kollyuchkaola@gmail.com>
2020-04-28 19:12:00 +0600

    redirect

--- forms/views.py
+++ forms/views.py
@@ -1,20 +1,25 @@
 from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 from .forms import Form_one
 
 
 class FormsView(View):
+
     def get(self,request):
         form = Form_one()
         return render(request,'form.html',{'form':form})
+
+
     def post(self,request):
         form = Form_one(request.POST,request.FILES)
         if form.is_valid():
 
 
             context = form.cleaned_data
-            # return render(request,'form.html',context)
+
             return HttpResponseRedirect('/form/')
+
         else:
             return render(request,'error.html',{"error":form.errors})
