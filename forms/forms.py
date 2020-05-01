84d184f0e0cbf2afdc45867f1718640ff9e31e5f
Kucherenko Olga <kollyuchkaola@gmail.com>
2020-05-01 11:57:25 +0300

    Update forms/forms.py
    Co-authored-by: Igor Rodionov  <goruha@gmail.com>

--- forms/forms.py
+++ forms/forms.py
@@ -6,7 +6,7 @@ class Form_one(forms.Form):
     file_3 = forms.FileField(label="С 5-ю слоями рафта в режиме вазы, сохраняем и загружаем. 3 файл")
     file_4 = forms.FileField(label="С 5-ю слоями рафта в обычном режиме, сохраняем и загружаем. 4 файл ")
 
-    stack_height = forms.IntegerField(label = 'STACK_HEIGHT',min_value = 0, max_value = 5, initial= 1)
+    stack_height = forms.IntegerField(label = 'STACK_HEIGHT',min_value = 1, max_value = 10, initial= 1)
     botton_solid_layer = forms.FloatField(label = 'BOTTOM_SOLID_LAYER',min_value = 10, max_value = 30, initial= 19.75)
     top_solid_layer = forms.FloatField(label = 'TOP_SOLID_LAYER',min_value = 10, max_value = 30, initial= 21.25)
     top_raft_layer = forms.FloatField(label = 'TOP_RAFT_LAYER',min_value = 0, max_value = 5, initial= 1.75)
